1578d1920245496756c185bb5558e3f0745fe0ce
fire855 <thefire855@gmail.com>
2017-08-24 13:52:51 +0200

    Add missing symbols for offline charging & omx

--- libmtkshims/Android.mk
+++ libmtkshims/Android.mk
@@ -13,3 +13,16 @@ LOCAL_SHARED_LIBRARIES := libui
 LOCAL_MODULE := libmtkshim_ui
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := gui.cpp
+LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_MODULE := libmtkshim_gui
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := omx.cpp
+LOCAL_MODULE := libmtkshim_omx
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
