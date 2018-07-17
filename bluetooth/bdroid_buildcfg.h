94e1ac93d69022fb562628b8df5f168599e159bf
Sean Hoyt <seanhoyt963@gmail.com>
2018-07-17 01:04:54 -0500

    Set a proper bluetooth name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME		"BlocksDuino"
+#define BTM_DEF_LOCAL_NAME		"Harmony Smartwatch"
 
 #define BTA_AV_MAX_A2DP_MTU  1788
 
