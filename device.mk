0c20687a6c995cddeb0c4c00a5597716355d129e
deadman96385 <seanhoyt963@gmail.com>
2018-08-05 22:02:33 -0500

    Build watch settings

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
+# Watch specific settings
+PRODUCT_PACKAGES += \
+    WatchSettings
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
